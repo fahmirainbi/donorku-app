fcf7382c05042f579df24b0ddf684cfe44d18098
Fahmi <fahmijamaludin2@gmail.com>
2023-04-13 12:59:56 +0700

    add asset xml 3

---
+++ app/src/main/java/com/example/donorku_app/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.donorku_app.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.donorku_app.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
