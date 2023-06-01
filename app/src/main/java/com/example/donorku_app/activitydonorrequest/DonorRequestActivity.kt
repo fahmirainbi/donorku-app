ec3de34f013e41eec4590f980ce30c738c7ed181
Fahmi <fahmijamaludin2@gmail.com>
2023-06-01 12:58:58 +0700

    add Donor Request Activity

--- app/src/main/java/com/example/donorku_app/activitydonorrequest/DonorRequestActivity.kt
+++ app/src/main/java/com/example/donorku_app/activitydonorrequest/DonorRequestActivity.kt
@@ -1,8 +1,12 @@
 package com.example.donorku_app.activitydonorrequest
 
+import android.app.DatePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.donorku_app.databinding.ActivityDonorRequestBinding
+import com.example.donorku_app.home.HomeActivity
+import java.util.Calendar
 
 class DonorRequestActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDonorRequestBinding
@@ -12,5 +16,23 @@ class DonorRequestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val view = binding.root
         setContentView(view)
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        binding.pickDateBtn.setOnClickListener {
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view,mYear,mMonth,mDay->
+                binding.dateTv.setText(""+mDay+"/"+mMonth+"/"+mYear)
+            },year,month,day)
+            dpd.show()
+        }
+
+        binding.ivBack.setOnClickListener{
+                startActivity(Intent(this, HomeActivity::class.java))
+        }
+
     }
+
 }
